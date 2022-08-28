d23c72d78c35e3d50dddc0ac299f3d94bc24cb95
Victor <victorddaramola@gmail.com>
2022-08-28 01:19:10 -0400

    To-Do list header and styling component

---
+++ src/components/ToDoHeader/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+function ToDoHeader() {
+  return (
+    <header className="header">
+      <div className="header-tab">
+        <h1 className="header-text">To-Do List</h1>
+      </div>
+    </header>
+  );
+}
+
+export default ToDoHeader;
