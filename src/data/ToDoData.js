00410cac3337d2b59b5723040992132d7632a7ee
Victor <victorddaramola@gmail.com>
2022-08-28 01:18:32 -0400

    To-Do list tasks

---
+++ src/data/ToDoData.js
@@ -0,0 +1,9 @@
+const ToDoData = [
+  { id: 0, task: "Walk the dog" },
+  { id: 1, task: "Swim in every ocean" },
+  { id: 2, task: "Go on a road trip" },
+  { id: 3, task: "Fly in a hot air balloon" },
+  { id: 4, task: "See the Aurora Borealis (Northern Lights)" },
+];
+
+export default ToDoData;
